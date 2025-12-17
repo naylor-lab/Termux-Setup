e03f454be6ec0d3b29cc8e61467d5a6db7abaa62
Open Source <naylor-laboratory@tutamail.com>
2025-12-17 09:29:01 -0300

    Criar desktop.sh

---
+++ src/install/desktop.sh
@@ -0,0 +1,7 @@
+cd ~
+
+wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/termux_native/startxfce4_termux.sh
+
+chmod +x ~/startxfce4_termux.sh
+
+bash ~/startxfce4_termux.sh
