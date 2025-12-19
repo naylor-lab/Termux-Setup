66080c323066496a266ff1d7a8bd3e48e5161042
Open Source <naylor-laboratory@tutamail.com>
2025-12-18 21:25:22 -0300

    Atualizar o default.sh

--- src/install/default.sh
+++ src/install/default.sh
@@ -2,12 +2,16 @@ pkg update -y
 pkg upgrade -y
 
 #Dependencies:
-pkg install termux-x11-nightly -y
+pkg install root-repo
+pkg install x11-repo
+
+pkg install termux-x11-nightly
+
+
 pkg install pulseaudio -y
 
 pkg install tur-repo -y
 pkg install firefox
 
-pkg install x11-repo
 pkg install wget -y
 pkg install xfce4 -y
